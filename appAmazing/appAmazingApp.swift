8e14436b24255f6f6853bec13f6cfa0340c137ed
Rudy Guerriero <taripowa@gmail.com>
2024-11-04 15:18:54 +0100

    Initial Commit

---
+++ appAmazing/appAmazingApp.swift
@@ -0,0 +1,17 @@
+//
+//  appAmazingApp.swift
+//  appAmazing
+//
+//  Created by Rita Guerriero on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct appAmazingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
